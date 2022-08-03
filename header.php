6eefbf5d277642171b9d82c5dcbb841dee17699f
Danil <marfol@inbox.ru>
2022-08-03 12:36:43 +0300

    sss

---
+++ header.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <meta name="viewport" content="width"> -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;700;900&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/custom-select.css">
+    <link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css"
+    />
+
+
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.13.1/themes/base/jquery-ui.css">
+    <link rel="stylesheet" href="/resources/demos/style.css">
+
+
+
+    <title>Document</title>
+</head>
+<body>
+
+    <header>
+        <div class="container">
+            <div class="header">
+                <div class="header-left">
+                    <a href="/" class="logo">
+                        <img src="assets/images/header/logo.png" alt="">
+                    </a>
+                </div>
+                <div class="header-center">
+                    <ul class="header-menu">
+                        <li><a href="about.html">О центре</a></li>
+                        <li><a href="news.html">Новости</a></li>
+                        <li><a href="contacts.html">Контакты</a></li>
+                    </ul>
+                </div>
+                <div class="header-right">
+                    <ul class="header-menu">
+                        <li>
+                            <div class="icon">
+                                <img src="assets/images/header/icon1.png" alt="">
+                            </div>
+                            <a href="#">ЦКР</a>
+                        </li>
+                        <li>
+                            <div class="icon">
+                                <img src="assets/images/header/icon2.png" alt="">
+                            </div>
+                            <a href="#">РЦИ</a>
+                        </li>
+                        <li>
+                            <div class="icon">
+                                <img src="assets/images/header/icon3.png" alt="">
+                            </div>
+                            <a href="#">РЦК</a>
+                        </li>
+                        <li>
+                            <div class="icon">
+                                <img src="assets/images/header/icon4.png" alt="">
+                            </div>
+                            <a href="#">Камский кластер</a>
+                        </li>
+                        <li>
+                            <div class="icon">
+                                <img src="assets/images/header/icon5.png" alt="">
+                            </div>
+                            <a href="#">Промпарки</a>
+                        </li>
+                    </ul>
+                    <div class="search">
+                        <img src="assets/images/header/search.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
