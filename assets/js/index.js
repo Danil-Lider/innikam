053503e325104729c17063d2ba333ee35497a7ec
Danil <marfol@inbox.ru>
2022-08-04 11:18:30 +0300

    Complited page claster-6_inno_rck

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,59 +1,20 @@
 //  ПОЛУЧАЕМ ВСЕ ЭЛЕМЕНТЫ
-let items = document.querySelectorAll('.block-open');
+let items = document.querySelectorAll('.item-btn-check');
 
-for (let elem of items) {
+items.forEach(element => {
 
+    element.addEventListener( "click" , function(e){
 
-    // .querySelector('.open-items')
+        items.forEach(el => {console.log(el.classList.remove('active'))});
+        element.classList.add('active')
 
-    let main_elem_click = elem
-    let all_items = elem.querySelector('.all-items')
+        var active_id = this.getAttribute('data-block_id')
+        var active_block = document.querySelector('.block_id-'+ active_id);
 
-    
+        document.querySelectorAll('.Our-support-methods').forEach(element => { element.style.display = "none";})
+        active_block.style.display = "block";
 
-    main_elem_click.addEventListener("click", function (e) {
-        
-
-        if(e.target.classList =='open-items'){
-            e.preventDefault()
-        }
-
-        // e.preventDefault()123
-
-        all_items.classList.toggle('active');
-        
-    })
-}
+    });
+});
 
 
-//  FOR PAGE NUMBER 5  frequently-asked-questions
-
-
-
-let items_often_asked_questions = document.querySelectorAll('.frequently-asked-questions .item');
-
-for (let elem of items_often_asked_questions) {
-
-    console.log(elem)
-
-    let main_elem_click = elem.querySelector('.image')
-    let all_items = elem.querySelector('.text')
-
-    // let all_items_images = elem.querySelectorAll('.image')
-
-    main_elem_click.addEventListener("click", function (e) {
-
-        e.preventDefault()
-        all_items.classList.toggle('active');
-
-        if(all_items.classList.contains('active')){
-            main_elem_click.querySelector('.not-active').classList.add('active')
-            main_elem_click.querySelector('.active').classList.remove('active');
-        }else{
-            main_elem_click.querySelector('.not-active').classList.remove('active')
-            main_elem_click.querySelector('.active-first').classList.add('active');
-        }
-
-    })
-
-}
